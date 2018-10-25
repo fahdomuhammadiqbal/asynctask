ae10a805b99e5a71327f8e527226711574a6ce90
LabKom <labkom@gmail.com>
2018-10-25 10:58:30 +0700

    tugas

---
+++ app/src/main/java/labsi1/co/id/whowroteit_fahdo/MainActivity.java
@@ -0,0 +1,6 @@
+package labsi1.co.id.whowroteit_fahdo;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/labsi1/co/id/whowrotelt_fahdo/BookLoader.java
@@ -0,0 +1,28 @@
+package labsi1.co.id.whowrotelt_fahdo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return labsi1.co.id.whowrotelt_fahdo.NetworkUtils.getBookInfo(mQueryString);
+    }
+}
